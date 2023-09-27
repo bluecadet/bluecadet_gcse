738130812b7aaa3d661b82ebe18f453b915d5b1e
Pete Inge <pinge@bluecadet.com>
2023-09-27 15:28:28 -0400

    Update node stuff to simplify everything

--- bldrConfig.js
+++ bldrConfig.js
@@ -9,17 +9,17 @@ let js_config = [];
 let images_config = [];
 
 drupal_modules.forEach((val, i) => {
-  // css_config.push({
-  //   src: val + 'assets/src/css/**/*.css',
-  //   dest: val + 'assets/dist/css',
-  //   watch: [val + 'assets/src/css/**/*.css'],
-  // });
+  css_config.push({
+    src: val + 'assets/src/css/**/*.css',
+    dest: val + 'assets/dist/css',
+    watch: [val + 'assets/src/css/**/*.css'],
+  });
 
-  // sass_config.push({
-  //   src: val + 'assets/src/scss/*.scss',
-  //   dest: val + 'assets/dist/css',
-  //   watch: [val + 'assets/src/scss/**/*.css'],
-  // });
+  sass_config.push({
+    src: val + 'assets/src/scss/*.scss',
+    dest: val + 'assets/dist/css',
+    watch: [val + 'assets/src/scss/**/*.css'],
+  });
 
   js_config.push({
     src: val + 'assets/src/js/**/*.js',
@@ -27,18 +27,18 @@ drupal_modules.forEach((val, i) => {
     watch: [val + 'assets/src/js/**/*.js'],
   });
 
-  // images_config.push({
-  //   src: val + 'assets/src/images/*.{jpg,JPG,jpeg,JPEG,gif,png,svg}',
-  //   dest: val + 'assets/dist/images',
-  //   watch: [val + 'assets/src/images/**/*'],
-  // });
+  images_config.push({
+    src: val + 'assets/src/images/*.{jpg,JPG,jpeg,JPEG,gif,png,svg}',
+    dest: val + 'assets/dist/images',
+    watch: [val + 'assets/src/images/**/*'],
+  });
 });
 
 module.exports = {
   css: css_config,
   sass: sass_config,
   js: js_config,
-  images: [], //images_config
+  images: images_config,
   // --------------------- END BASIC CONFIG --------------------- //
   rollup: {
     outputOptions: {
